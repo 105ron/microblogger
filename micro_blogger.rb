8a2f9a41da408aad338e1404d34d09d04122dcfe
rhys <rbrooker105@gmail.com>
2016-09-02 20:19:30 +1000

    account verified, posting to twiiter account successfully

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -5,5 +5,24 @@ class MicroBlogger
 
   def initialize
     puts "Initializing MicroBlogger"
+    @client = JumpstartAuth.twitter
   end
+
+  def run
+    puts "Welcome to the JSL Twitter Client!"
+  end
+
+
+  def tweet(message)
+  	if (message.length) <= 140
+      @client.update(message)
+	else
+	  puts "Message length greater than 140 characters"
+
+	end
+  end
+
 end
+blogger = MicroBlogger.new
+blogger.tweet("MicroBlogger Initialized")
+
